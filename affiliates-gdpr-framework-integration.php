dbe2d18c1ad658eaa979f7231c1dae6e49b6ee8f
geotsiokos <gtsiokos@gmail.com>
2019-03-08 18:14:36 +0200

    compatible with Affiliates 4, updated requirement inclusion

--- affiliates-gdpr-framework-integration.php
+++ affiliates-gdpr-framework-integration.php
@@ -3,7 +3,7 @@
  * Plugin Name: Affiliates GDPR Framework integration
  * Plugin URI: http://www.netpad.gr
  * Description: Integrates Affiliates plugin with GDPR Framework integration.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: George Tsiokos
  * Author URI: http://www.netpad.gr
  */
@@ -18,12 +18,8 @@ add_action( 'plugins_loaded', 'aff_gdpr_check_dependencies' );
  * Check plugin dependencies
  */
 function aff_gdpr_check_dependencies() {
-	$active_plugins = get_option( 'active_plugins', array() );
-	$affiliates_is_active = in_array( 'affiliates/affiliates.php', $active_plugins ) || in_array( 'affiliates-pro/affiliates-pro.php', $active_plugins ) || in_array( 'affiliates-enterprise/affiliates-enterprise.php', $active_plugins );
-	$gdpr_framework_is_active = in_array( 'gdpr-framework/gdpr-framework', $active_plugins );
-
 	if ( defined( 'AFFILIATES_PLUGIN_DOMAIN' ) && defined( 'GDPR_FRAMEWORK_VERSION' ) ) {
-		add_filter( 'affiliates_registration_after_fields', 'example_affiliates_registration_after_fields' );
+		add_filter( 'affiliates_registration_after_fields', 'affiliates_gdpr_registration_after_fields' );
 	}
 }
 
@@ -33,10 +29,10 @@ function aff_gdpr_check_dependencies() {
  * @param string $output
  * @return string $output
  */
-function example_affiliates_registration_after_fields( $output ) {
+function affiliates_gdpr_registration_after_fields( $output ) {
 	$output = '';
 	if ( !function_exists( 'gdpr' ) ) {
-		include_once ( ABSPATH . 'wp-content/plugins/gdpr-framework/bootstrap.php' );
+		include_once ( plugin_dir_path( __DIR__ ) . 'gdpr-framework.php/bootstrap.php' );
 	}
 	$privacy_policy = get_option( 'gdpr_policy_page' );
 	$terms_page = get_option( 'gdpr_terms_page' );
